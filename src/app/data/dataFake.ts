6eabe8343f170411ef7d8711b154e8337da08c05
Willj92 <wilsonj92@hotmail.com>
2023-05-30 09:11:04 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title":"Nova versão Angular a vista!",
+    "description": "Já está no ar a versão 16 do Angular. Acompanhe tudo sobre a nova atualização do framework aqui com a gente",
+    "photoCover": "https://miro.medium.com/v2/resize:fit:1400/1*CNy2EclZxC1hmi_govb_7w.png"
+  },
+  {
+    "id": "2",
+    "title":"Saiba tudo sobre a produção da saga de Senhor dos Anéis",
+    "description": "pipipi popo po",
+    "photoCover": "https://s.aficionados.com.br/imagens/o-senhor-dos-aneis-qual-a-ordem-correta-para-assistir-filmes-e-serie_f.jpg"
+  }
+]
